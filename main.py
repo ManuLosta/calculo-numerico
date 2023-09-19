01d870de26542ce8417f6b5b9ef82a5da314240f
ManuLosta <mlostalom@gmail.com>
2023-09-19 07:46:18 -0300

    nuevos ejs

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import math
 from equation import bisection, newton_raphson
 
 if __name__ == '__main__':
-    f1 = lambda x: math.e**(-x) - x + 2
-    print(bisection(f1, 2, 3, 10e-15))
+    f1 = lambda x: x-2**(-x)
+    f1d = lambda x: 1 + math.log(2)*2**(-x)
+    print(newton_raphson(f1, f1d, -0.5, 10e-8))
+
